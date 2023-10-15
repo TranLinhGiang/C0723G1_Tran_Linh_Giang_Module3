8b4039d354948d998a369524e39ff2d8f6043b78
Trần Linh Giang <gt07839@gmail.com>
2023-10-15 16:09:31 +0700

    Case_study

--- case_study/tast 5.sql
+++ case_study/tast 5.sql
@@ -1,30 +1,23 @@
 use case_study;
 -- hiển thị mã khách hàng , họ tên, tên loại khách, mã hợp đồng, tên dịch vụ, ngày làm hợp đồng, ngày kết thúc, tổng tiền;
 -- công thức tinsg tổng số tiền: chi phí thuê + số lượng * giá 
-SELECT 
-    khach_hang.ma_khach_hang,
-    khach_hang.ho_ten,
-    loai_khach.ten_loai_khach_hang,
-    dich_vu.ten_dich_vu,
-    hop_dong.ma_hop_dong,
-    hop_dong.ngay_lam_hop_dong,
-    hop_dong.ngay_ket_thuc,
-    SUM(dich_vu_di_kem.gia * hop_dong_chi_tiet.so_luong) AS tong_tien
-FROM
-    khach_hang
-        JOIN
-    loai_khach ON khach_hang.ma_loai_khach = loai_khach.ten_loai_khach_hang
-        LEFT JOIN
-    hop_dong ON hop_dong.ma_hop_dong
-        AND hop_dong.ngay_lam_hop_dong
-        AND hop_dong.ngay_ket_thuc
-        LEFT JOIN
-    dich_vu ON dich_vu.ten_dich_vu
-    left join 
-    hop_dong_chi_tiet on hop_dong_chi_tiet.so_luong
-    left join 
-    dich_vu_di_kem on dich_vu_di_kem.gia;
-    
+
+select 
+khach_hang.ma_khach_hang,
+khach_hang.ho_ten,
+loai_khach.ten_loai_khach_hang,
+hop_dong.ma_hop_dong,
+dich_vu.ten_dich_vu,
+hop_dong.ngay_lam_hop_dong,
+hop_dong.ngay_ket_thuc,
+dich_vu.chi_phi_thue + hop_dong_chi_tiet.so_luong * dich_vu_di_kem.gia as tong_tien
+from
+khach_hang 
+inner join loai_khach on khach_hang.ma_loai_khach = loai_khach.ma_loai_khach
+left join hop_dong on hop_dong.ma_khach_hang= khach_hang.ma_khach_hang
+left join dich_vu on hop_dong.ma_dich_vu= dich_vu.ma_dich_vu
+left join hop_dong_chi_tiet on hop_dong.ma_hop_dong= hop_dong_chi_tiet.ma_hop_dong
+left join dich_vu_di_kem on hop_dong_chi_tiet.ma_dich_vu_di_kem= dich_vu_di_kem.ma_dich_vu_di_kem;
     
  
     
