ccbbf23b9d0747d7c3e75e7af5eec114b4b50b3f
Trần Linh Giang <gt07839@gmail.com>
2023-10-15 14:45:52 +0700

    Giang_nop_bai_tap_thuc_hanh_ss2

--- ss2_thiet_ke_tao_csdl/bai_tap/bai_2/xay_dung_co_so_du_lieu_quan_ly_ban_hang.sql
+++ ss2_thiet_ke_tao_csdl/bai_tap/bai_2/xay_dung_co_so_du_lieu_quan_ly_ban_hang.sql
@@ -2,32 +2,32 @@ create database if not exists xay_dung_co_so_du_lieu_quan_ly_ban_hang;
 use  xay_dung_co_so_du_lieu_quan_ly_ban_hang;
 -- tạo bảng product;
 CREATE TABLE product (
-    pID INT PRIMARY KEY,
-    pName VARCHAR(50),
-    pPice int
+    product_id INT PRIMARY KEY,
+    product_name VARCHAR(50),
+    product_price int
 );
 -- xây dựng lớp order;
 CREATE TABLE `order` (
-    oID INT PRIMARY KEY,
-    cID INT,
-    oDate DATE,
-    oTotalPrice INT
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    order_total_price INT
 );
 -- tạo lớp orderDetail;
 CREATE TABLE order_detail (
-    order_oID INT,
-    Product_pID INT,
-    odQTY int,
-    PRIMARY KEY (order_oID , Product_pID),
-    FOREIGN KEY (order_oID) REFERENCES `order` (oID),
-    FOREIGN KEY (Product_pID) REFERENCES Product (pID)
+    order_order_id INT,
+    product_product_id INT,
+    order_quality int,
+    PRIMARY KEY (order_order_id , product_product_id),
+    FOREIGN KEY (order_order_id) REFERENCES `order` (order_id),
+    FOREIGN KEY (product_product_id) REFERENCES product (product_id)
 );
 -- tạo bảng customer
 CREATE TABLE customer (
-    order_oID INT,
-    cID int primary key,
-    cName VARCHAR(50),
-    cAge INT,
-    FOREIGN KEY (order_oID)REFERENCES `order` (oID)
+    order_order_id INT,
+    customer_id int primary key,
+    customer_name VARCHAR(50),
+    customer_age INT,
+    FOREIGN KEY (order_order_id)REFERENCES `order` (order_id)
 );
 
