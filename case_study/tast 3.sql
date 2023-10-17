da235754dd40e515fe6a2f4be38f91d0b7cf507a
Trần Linh Giang <gt07839@gmail.com>
2023-10-17 20:48:01 +0700

    Case_study

--- case_study/tast 3.sql
+++ case_study/tast 3.sql
@@ -3,8 +3,3 @@ use case_study;
 select * from khach_hang
 where (timestampdiff(year, ngay_sinh, current_date())between 18 and 50)
 and (dia_chi like '%Đà Nẵng%' or dia_chi like '%Quảng Trị%');
-
-
-
-
-  
