dc19c15824cc874821b78800f9199c0b0d0b7e14
Trần Linh Giang <gt07839@gmail.com>
2023-10-18 19:20:03 +0700

    Giang_nop_bai_tap_thuc_hanh_ss5

--- Module 3/case_study1/task2_to_task5/task2.sql
+++
@@ -1,9 +0,0 @@
--- Task 2 -- 
-
-/* Hiển thị thông tin của tất cả nhân viên có trong hệ thống có tên bắt đầu là một trong các ký tự “H”, “T” hoặc “K” và có tối đa 15 ký tự. */
-
-use furama_resort;
-
-select * from nhan_vien
-where ho_ten like 'H%' or ho_ten like 'T%' or ho_ten like 'K%'
-and length(ho_ten) <= 15;
